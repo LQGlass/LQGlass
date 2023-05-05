68f48c48acaf187cfcb505c169cca813f9fab4d7
unknown <yerif7@gmail.com>
2023-05-04 19:33:02 -0600

    Se agregaron validaciones

--- components/componentsPanel/PromocionesModal.jsx
+++ components/componentsPanel/PromocionesModal.jsx
@@ -30,15 +30,11 @@ const PromocionesModal = () => {
         links.push(url);
       }
       setUrls(links);
-      console.log(urls);
       if (refresh) {
         setRefreshUrls(false);
       }
     } catch (error) {
       console.error(error);
-      if (error.code === "storage/unknown") {
-        console.log("Detalles del error:", error.customData.serverResponse);
-      }
     }
   };
 
@@ -48,13 +44,35 @@ const PromocionesModal = () => {
 
   // Función para subir una imagen a Firebase Storage
   const handleImageUpload = async index => {
-    if (selectedImage) {
-      const imageName = `promo${index}`; // Se genera el nombre del archivo
-      const imageRef = ref(storage, `/imagenespromo/${imageName}`); // Se obtiene la referencia del archivo
-      await uploadBytes(imageRef, selectedImage); // Se sube la imagen al storage
-      setSelectedImage(null); // Limpiar el archivo seleccionado
-      setRefreshUrls(!refreshUrls); // Actualizar las URLs de las imágenes de promociones
+    if (!selectedImage) {
+      document.getElementById("message").innerHTML =
+        "No se ha seleccionado ningún archivo jpg.";
+      setTimeout(() => {
+        document.getElementById("message").innerHTML = "";
+      }, 5000);
+      return;
     }
+
+    // Validar el tamaño del archivo
+    if (selectedImage.size > 1000000) {
+      document.getElementById("message").innerHTML =
+        "El tamaño de la imagen es mayor a 1MB. Por favor, comprime la imagen en este <a href='https://www.iloveimg.com/es/comprimir-imagen' target='_blank'>enlace</a> y vuelve a intentarlo.";
+      setTimeout(() => {
+        document.getElementById("message").innerHTML = "";
+      }, 5000);
+      return;
+    }
+
+    const imageName = `promo${index}`; // Se genera el nombre del archivo
+    const imageRef = ref(storage, `/images/promociones/${imageName}.jpg`); // Se obtiene la referencia del archivo
+    await uploadBytes(imageRef, selectedImage).then(() => {
+      document.getElementById("message").innerHTML = "Imagen actualizada";
+      setTimeout(() => {
+        document.getElementById("message").innerHTML = "";
+      }, 2000);
+    });
+    setSelectedImage(null); // Limpiar el archivo seleccionado
+    setRefreshUrls(!refreshUrls); // Actualizar las URLs de las imágenes de promociones
   };
 
   return (
@@ -62,10 +80,11 @@ const PromocionesModal = () => {
       <Button onClick={() => setShow(true)}>Editar Promos</Button>
       <Modal show={show} onHide={() => setShow(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>Editar promociones</Modal.Title>
+          <Modal.Title>Editar promociones (jpg)</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <div>
+            <p id="message"></p>
             {urls.map((url, index) => (
               <div key={index}>
                 <Image
@@ -77,10 +96,14 @@ const PromocionesModal = () => {
                 <input
                   type="file"
                   onChange={e => setSelectedImage(e.target.files[0])}
+                  accept="image/jpg"
                 />
-                <button onClick={() => handleImageUpload(index + 1)}>
-                  Actualiza promo {index + 1}
-                </button>
+                <Button
+                  variant="dark"
+                  onClick={() => handleImageUpload(index + 1)}
+                >
+                  Actualiza {index + 1}
+                </Button>
               </div>
             ))}
           </div>
