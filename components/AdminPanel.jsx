08fce8eca57278dceb57132e09e43e5daa798f7b
unknown <yerif7@gmail.com>
2023-05-07 22:58:02 -0600

    Se corrigió error en icono de Covid

--- components/AdminPanel.jsx
+++ components/AdminPanel.jsx
@@ -7,7 +7,9 @@ export default function AdminPanel() {
         <Link href="/panel-admin">Ir a panel de administrador</Link>
       </p>
       <p>
-        <Link href="/manual-usuario">Manual de usuario</Link>
+        <Link href="https://firebasestorage.googleapis.com/v0/b/lqglasscms.appspot.com/o/manuales%2FManualenviodocumentos_compressed.pdf?alt=media&token=193b6494-4abb-41ed-a234-90a8355612a4">
+          Manual para subir resultados y facturas.
+        </Link>
       </p>
     </>
   );
