fc19e5205a9e98e60ff283e90a1b2c6099d031ef
unknown <yerif7@gmail.com>
2023-05-07 23:24:21 -0600

    Se agrego sharp y se arreglaron bugs en imagenes

--- components/BlogSeccionExpanded.jsx
+++ components/BlogSeccionExpanded.jsx
@@ -10,19 +10,19 @@ function BlogSeccion() {
         <CasillaBlog
           titulo="Análisis de laboratorio: clave en la prevención de enfermedades"
           descripcion="Los análisis de laboratorio son una herramienta esencial en la prevención de enfermedades en todas las edades. Los análisis de sangre, orina, heces, entre otros, son útiles para detectar enfermedades tempranamente, lo que permite un tratamiento oportuno y eficaz. "
-          url="/images/blog/blog.jpg"
+          urlImagenBlog="/images/blog/blog.jpg"
           link="/blog/articulo1"
         />
         <CasillaBlog
           titulo="La importancia de los análisis preventivos en la detección temprana de enfermedades"
           descripcion="Los análisis preventivos son herramientas esenciales para la detección temprana de enfermedades. A menudo, las personas acuden al médico solo cuando experimentan síntomas, pero los análisis preventivos pueden ayudar a identificar problemas de salud antes de que se manifiesten. "
-          url="/images/blog/blog1.jpg"
+          urlImagenBlog="/images/blog/blog1.jpg"
           link="/blog/articulo2"
         />
         <CasillaBlog
           titulo="Los análisis de laboratorio más solicitados para la prevención de enfermedades crónicas"
           descripcion="Los análisis de laboratorio son una herramienta esencial para la prevención de enfermedades crónicas. Muchas enfermedades crónicas pueden ser prevenidas o controladas tempranamente a través de la detección de biomarcadores específicos en los análisis de laboratorio."
-          url="/images/blog/blog2.jpg"
+          urlImagenBlog="/images/blog/blog2.jpg"
           link="/blog/articulo3"
         />
       </div>
